253becccfb32c29b563a132244a17a8e394efc61
Jon Rey Galera <jonreygalera@gmail.com>
2025-11-27 07:44:33 +0800

    clean up

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,6 @@ import kernel from './core/kernel';
 import config from './config/app.config';
 import { errLog, infoLog } from './core/utils/logger.util';
 
-// Application entry point - initializes and starts the Express server
-// (async () => {
-//   try {
-//     const server = await kernel();
-//     server?.listen(config.port, () => {
-//      infoLog(`Listening to port: ${config.port}`);
-//     });
-//   } catch (error) {
-//     errLog('Failed to start the server:', error);
-//     process.exit(1); // Exit the process with a failure code
-//   }
-// })();
-
 const serverPromise = kernel();
 
 serverPromise.then(server => {
