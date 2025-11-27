5ab78e0646053869efb3f828ffe9c46b1f1471e5
Jon Rey Galera <jonreygalera@gmail.com>
2025-11-27 11:05:20 +0800

    clean up vercel

--- src/server.ts
+++ src/server.ts
@@ -8,41 +8,41 @@ import appConfig from './config/app.config';
 
 const app = express();
 
-let isInitialized = false;
+// let isInitialized = false;
 
-const initializeApp = async () => {
-  if (isInitialized) return app;
-  try {
-    const appKernel: IAppKernel = {
-      appExpress: app,
-      database: DatabaseCore.getInstance()
-    };
+// const initializeApp = async () => {
+//   if (isInitialized) return app;
+//   try {
+//     const appKernel: IAppKernel = {
+//       appExpress: app,
+//       database: DatabaseCore.getInstance()
+//     };
 
-    await kernel(appKernel);
-    isInitialized = true;
+//     await kernel(appKernel);
+//     isInitialized = true;
 
-    // For Vercel: Do not listen here, just initialize
-    if (!appConfig.severless) {
-      appKernel.appExpress.listen(config.port, () => {
-        infoLog(`Listening to port: ${config.port}`);
-      });
-    } else {
-      infoLog(`Running in serverless or Vercel environment`);
-    }
+//     // For Vercel: Do not listen here, just initialize
+//     if (!appConfig.severless) {
+//       appKernel.appExpress.listen(config.port, () => {
+//         infoLog(`Listening to port: ${config.port}`);
+//       });
+//     } else {
+//       infoLog(`Running in serverless or Vercel environment`);
+//     }
 
-    return appKernel.appExpress;
-  } catch (error) {
-    errLog('Failed to initialize the server:', error);
-    throw error;
-  }
-};
+//     return appKernel.appExpress;
+//   } catch (error) {
+//     errLog('Failed to initialize the server:', error);
+//     throw error;
+//   }
+// };
 
-if (!appConfig.severless) {
-  // Only start server if not in serverless mode and not on Vercel
-  initializeApp();
-} else {
-  // For Vercel, export a handler (the default export)
-  initializeApp();
-}
+// if (!appConfig.severless) {
+//   // Only start server if not in serverless mode and not on Vercel
+//   initializeApp();
+// } else {
+//   // For Vercel, export a handler (the default export)
+//   initializeApp();
+// }
 
 export default app;
