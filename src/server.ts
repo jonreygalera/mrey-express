88cb031e81eb1a7688e2a89f2fc79e241ca15114
Jon Rey Galera <jonreygalera@gmail.com>
2025-11-27 08:39:55 +0800

    clean up vercel

--- src/server.ts
+++ src/server.ts
@@ -5,22 +5,24 @@ import { errLog, infoLog } from './core/utils/logger.util';
 import IAppKernel from './core/types/appKernel.type';
 import DatabaseCore from './core/database.core';
 
-export const appKernel : IAppKernel = {
-  appExpress: express(),
-  database: DatabaseCore.getInstance()
-};
+const app = express();
 
 (async () => {
   try {
+    const appKernel : IAppKernel = {
+      appExpress: app,
+      database: DatabaseCore.getInstance()
+    };
+
     await kernel(appKernel);
     // UNCOMMENT FOR non-vercel prod
-    // appKernel.appExpress.listen(config.port, () => {
-    //   infoLog(`Listening to port: ${config.port}`);
-    // });
+    appKernel.appExpress.listen(config.port, () => {
+      infoLog(`Listening to port: ${config.port}`);
+    });
   } catch (error) {
     errLog('Failed to start the server:', error);
     process.exit(1);
   }
 })();
 
-export default appKernel.appExpress;
+export default app;
