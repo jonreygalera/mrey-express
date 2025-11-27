9c915d73064a06d2b68a9083c54a268172162e02
Jon Rey Galera <jonreygalera@gmail.com>
2025-11-27 08:09:08 +0800

    clean up vercel

--- src/core/kernel.ts
+++ src/core/kernel.ts
@@ -1,15 +1,10 @@
-import express, { Express } from 'express';
-import DatabaseCore from './database.core';
+import { Express } from 'express';
 import RegisterProvider from '../app/providers/register.provider';
 import IAppKernel from './types/appKernel.type';
 import { errLog } from './utils/logger.util';
 
-export const appKernel : IAppKernel = {
-  appExpress: express(),
-  database: DatabaseCore.getInstance()
-};
 
-export default async function(): Promise<Express | void> {
+export default async function(appKernel: IAppKernel): Promise<Express | void> {
   try {
     // Database connection initialization with error handling
     await appKernel.database.createConnection();
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
-import express from "express";
+import express from 'express';
 import kernel from './core/kernel';
 import config from './config/app.config';
 import { errLog, infoLog } from './core/utils/logger.util';
+import IAppKernel from './core/types/appKernel.type';
+import DatabaseCore from './core/database.core';
 
-const serverPromise = kernel();
+export const appKernel : IAppKernel = {
+  appExpress: express(),
+  database: DatabaseCore.getInstance()
+};
 
-serverPromise.then(server => {
-  if (server) {
-    server.listen(config.port, () => {
+(async () => {
+  try {
+    await kernel(appKernel);
+    appKernel.appExpress.listen(config.port, () => {
       infoLog(`Listening to port: ${config.port}`);
     });
+  } catch (error) {
+    errLog('Failed to start the server:', error);
+    process.exit(1);
   }
-}).catch(error => {
-  errLog('Failed to start the server:', error);
-  process.exit(1); // Exit the process with a failure code
-});
-
-export default express();
+})();
