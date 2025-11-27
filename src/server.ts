7a984f5fdf5e8f65617a03f78e44a7c2607d8fe2
Jon Rey Galera <jonreygalera@gmail.com>
2025-11-27 11:36:23 +0800

    clean up vercel

--- src/server.ts
+++ src/server.ts
@@ -1,59 +1,12 @@
-
-// let isInitialized = false;
-
 import { appConfig } from "./config";
-// import app from "./core/app";
-import { infoLog } from "./core/utils/logger.util";
 import webRoutes from './routes/web.routes';
 import express, { Express } from 'express';
-// const initializeApp = async () => {
-//   if (isInitialized) return app;
-//   try {
-//     const appKernel: IAppKernel = {
-//       appExpress: app,
-//       database: DatabaseCore.getInstance()
-//     };
-
-//     await kernel(appKernel);
-//     isInitialized = true;
-
-//     // For Vercel: Do not listen here, just initialize
-//     if (!appConfig.severless) {
-//       appKernel.appExpress.listen(config.port, () => {
-//         infoLog(`Listening to port: ${config.port}`);
-//       });
-//     } else {
-//       infoLog(`Running in serverless or Vercel environment`);
-//     }
-
-//     return appKernel.appExpress;
-//   } catch (error) {
-//     errLog('Failed to initialize the server:', error);
-//     throw error;
-//   }
-// };
-
-// if (!appConfig.severless) {
-//   // Only start server if not in serverless mode and not on Vercel
-//   initializeApp();
-// } else {
-//   // For Vercel, export a handler (the default export)
-//   initializeApp();
-// }
-
-// const appKernel: IAppKernel = {
-//   appExpress: app,
-//   database: DatabaseCore.getInstance()
-// };
-
-
-// kernel(appKernel);
-
-// export default app;
-
-// app.listen(appConfig.port, () => {
-//   infoLog(`Listening to port: ${appConfig.port}`);
-// });
 
 const app = express();
 app.use('/', webRoutes);
+
+const PORT = appConfig.port || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
